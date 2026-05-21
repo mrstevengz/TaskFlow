9f209be3bd4340abdad2472e5c61e082892bafac
gague-uam <ggpaiz@uamv.edu.ni>
2026-05-21 17:42:35 -0600

    Se empieza TaskDetailScreen

--- app/src/main/java/com/example/taskflow/screens/TaskDetailScreen.kt
+++ app/src/main/java/com/example/taskflow/screens/TaskDetailScreen.kt
@@ -2,9 +2,27 @@ package com.example.taskflow.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.google.firebase.database.core.Tag
 
 @Composable
 fun TaskDetailScreen(
