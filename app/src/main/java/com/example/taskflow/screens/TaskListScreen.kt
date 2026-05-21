b60241aa20268391648372a221f119fd48ef0d1b
gague-uam <ggpaiz@uamv.edu.ni>
2026-05-21 17:34:37 -0600

    TaskViewModel en proceso

--- app/src/main/java/com/example/taskflow/screens/TaskListScreen.kt
+++ app/src/main/java/com/example/taskflow/screens/TaskListScreen.kt
@@ -1,2 +1,71 @@
 package com.example.taskflow.screens
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskListScreen(
+    navController: NavController,
+    viewModel: TaskViewModel = viewModel()
+) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+        ,floatingActionButton = {
+            FloatingActionButton(onClick = {
+                navController.navigate(TaskDetail)
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Agregar tarea"
+                )
+            }
+        }
+    )
+    { padding ->
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Lista de tareas",
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        if (viewModel.tasks.isEmpty()) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text("No hay tareas registradas")
+            }
+        }
+        else {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+                    .padding(padding)
+                    .padding(16.dp)
+            )
+            {
+                items(viewModel.tasks.size)
+                {
+                    Card(
+                        modifier = Modifier.padding(vertical = 8.dp)
+                            .fillMaxWidth()
+                    )
+                    {
+                        Row(
+                            modifier = Modifier.padding(2.dp).fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                            ,verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(viewModel.tasks[it].id.toString())
+                            Text(viewModel.tasks[it].title)
+                            Checkbox(
+                                checked = viewModel.tasks[it].completed,
+                                onCheckedChange = {}
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
